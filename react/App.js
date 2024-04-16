9fd46cd174988561adeded638499b295f652c5d6
A1Coffee <166497232+A1Coffee@users.noreply.github.com>
2024-04-16 12:21:20 +0300

    Add files via upload

---
+++ react/App.js
@@ -0,0 +1,63 @@
+import React from "react"
+// import Header from "./components/Header"
+
+class App extends React.Component {
+  render(){
+    
+    return (<div className="name">
+      
+      <div class="heada">
+        <div class="overlay" />
+          <div class="iconufu"/>
+          <div class="logo2"/>
+          <div class="logo3"/>
+          <div class="button6">Главная</div>
+          <div class="button6">Направления</div>
+          <div class="button6">О школе</div>
+          <div class="menu1"/>
+      </div>
+      <div class="photoback"/>
+      <div class="motivat">
+        <h1>Погрузимся в мир <br/> информационных <br/> технологий вместе!</h1>
+      </div>
+      <div class="motivat2">
+        <p>Возрастная категория 12+</p>
+      </div>
+      <div class="zapis">
+        <p>Записаться</p>
+      </div><div class="startpng"/>
+      <div class="starttxt">Посмотрите<br/>видео про учебу<br/>в нашей школе</div>
+      <div class="rectangle">
+        <div class="circle"><h1>9</h1> <br/>направлений</div>
+        <div class="circle"><h1>7</h1> <br/>интенсивов</div>
+        <div class="circle"><h1>1000</h1><br/>учеников</div>
+        <div class="circle"><h1>3</h1><br/>года на рынке</div>
+      </div>
+      <div class="rectangle2">
+        <div class="row">
+          <div class="cube"><div class="book"/></div>
+          <div class="cube"><div class="m1"/></div>
+          <div class="cube"><div class="m2"/></div>
+        </div>
+        <div class="row">
+          <div class="cube">1</div>
+          <div class="cube">2</div>
+          <div class="cube">3</div>
+        </div>
+        <div class="row">
+          <div class="cube">4</div>
+          <div class="cube">5</div>
+          <div class="cube">6</div>
+        </div>
+      </div>
+      <div class="rectangle3">
+        <div class="rectangle3text">А ещё... у нас есть интенсивы!</div>
+        <div class="inrect3-1"></div>
+        <div class="inrect3-2"></div>
+        <div class="inrect3-3"></div>
+      </div>
+    </div>)
+  }
+}
+
+export default App
